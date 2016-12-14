4ff676971946b0af38c10a72a62d7fd75fde16b3
Sofia Vistas <sofia.vistas@blacklane.com>
2016-12-14 15:41:15 +0100

    Set up API accounts

---
+++ app/src/main/java/googleplayservices/android/svistas/com/recomendations/api/Etsy.java
@@ -0,0 +1,10 @@
+package googleplayservices.android.svistas.com.recomendations.api;
+
+/**
+ * Created by svistas on 14/12/16.
+ */
+
+public class Etsy {
+
+    public static final String API_KEY = "v6gn9z3q3kcjeko3jal27z01";
+}
