98fd25dbb04fd007df29e4862d3472bcb60429c9
Sofia Vistas <sofia.vistas@blacklane.com>
2016-12-20 18:37:34 +0100

    Handle error state

--- app/src/main/java/googleplayservices/android/svistas/com/recomendations/google/GoogleServicesHelper.java
+++ app/src/main/java/googleplayservices/android/svistas/com/recomendations/google/GoogleServicesHelper.java
@@ -1,6 +1,8 @@
 package googleplayservices.android.svistas.com.recomendations.google;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -36,11 +38,17 @@ public class GoogleServicesHelper implements GoogleApiClient.ConnectionCallbacks
     }
 
     public void connect(){
-        apiClient.connect();
+
+        if(isGooglePlayServicesAvailable()) {
+            apiClient.connect();
+        } else listener.onDisconnected();
     }
 
     public void disconnect(){
-        apiClient.disconnect();
+
+        if(isGooglePlayServicesAvailable()) {
+            apiClient.disconnect();
+        } else listener.onDisconnected();
     }
 
     public boolean isGooglePlayServicesAvailable(){
@@ -61,17 +69,33 @@ public class GoogleServicesHelper implements GoogleApiClient.ConnectionCallbacks
 
     @Override
     public void onConnected(Bundle bundle) {
-
+        listener.onConnected();
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        listener.onDisconnected();
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
+        if(connectionResult.hasResolution()){
+            try {
+                connectionResult.startResolutionForResult(activity, REQUEST_CODE_RESOLUTION);
+            } catch (IntentSender.SendIntentException e) {
+                connect();
+            }
+        } else listener.onDisconnected();
+    }
 
+    public void handleActivityRequest(int requestCode, int responseCode, Intent data){
+        if( requestCode == REQUEST_CODE_RESOLUTION || requestCode == REQUEST_CODE_AVAILABILITY){
+            if(responseCode == Activity.RESULT_OK){
+                connect();
+            } else {
+                listener.onDisconnected();
+            }
+        }
     }
 }
 
